30ebd49c9f610210085bb69645db30d711265372
BcodezMUJJE <billbrightmatege@gmail.com>
2024-02-14 10:08:44 +0000

    user_session

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+The User session module.
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """
+    The User session class.
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """
+        This Initializes a User session instance.
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
